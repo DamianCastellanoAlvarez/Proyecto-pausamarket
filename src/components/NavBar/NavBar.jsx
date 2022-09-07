5331a0e3641fabf1f07aba5ddb03109a1538b976
unknown <DamianCastellanoAlvarez>
2022-09-07 19:27:15 -0300

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
+import logo from '../../assets/Logo.png'
+import { Link } from "react-router-dom"
+import '../NavBar/NavBar.css'
+import CartWidget from "../CartWidget/CartWidget.jsx"
+
+
+const NavBar = () =>{
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    
+
+    const toggle = () => setDropdownOpen(!dropdownOpen);
+
+    return (
+        <>
+            <div>
+                <img src={logo} alt="logo"  className="logo" />
+            </div>
+        <div className='d-flex justify-content-end barra'>
+        <Nav tabs>
+            <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle nav caret>
+            <Link to="/">Productos</Link>
+            </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem enabled><Link className='barra' to='/productos/yerbas' >Yerba</Link></DropdownItem>
+                <DropdownItem enabled><Link className='barra' to='/productos/stevia'>Stevia</Link></DropdownItem>
+                <DropdownItem enabled><Link className='barra' to='/productos/te'>Te</Link></DropdownItem>
+            </DropdownMenu>
+            </Dropdown>
+            <NavItem>
+            <NavLink href='/blog'>Blog</NavLink>
+            </NavItem>
+            <NavItem>
+            <NavLink href='/contacto'>Contacto</NavLink>
+            </NavItem>
+            <CartWidget />
+        </Nav>
+        
+    <div>
+        
+        
+    </div>
+    </div>
+    </>
+    )
+}
+
+export default NavBar;
